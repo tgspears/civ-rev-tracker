becb51e16f1356a8418a0427ed955fb2fa5b490c
Trevor Spears <tg.spears@gmail.com>
2017-10-14 22:22:18 -0700

    troubleshooting Material and Materialize implementation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { MatMenuModule } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterializeModule } from "angular2-materialize";
+import { CRTNavComponent } from "./nav/nav.component";
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CRTNavComponent
   ],
   imports: [
     BrowserModule,
-    MaterializeModule
+    MaterializeModule,
+    BrowserAnimationsModule,
+    MatMenuModule
   ],
   providers: [],
   bootstrap: [AppComponent]
